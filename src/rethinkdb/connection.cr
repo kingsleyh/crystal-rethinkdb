f81c4910786a55429f6f1583d592a91b5fb9985b
Kingsley <kingsleyhendrickse@me.com>
2018-10-13 10:47:59 +0100

    updates

--- src/rethinkdb/connection.cr
+++ src/rethinkdb/connection.cr
@@ -22,7 +22,7 @@ class ConnectionResponse
   end
 end
 
-class ScrumAuthMessage1
+class AuthMessage1
   def initialize(@protocol_version : Int32, @authentication_method : String, @authentication : String)
   end
   JSON.mapping(
@@ -32,7 +32,7 @@ class ScrumAuthMessage1
   )
 end
 
-class ScrumAuthErrorResponse
+class AuthMessage1ErrorResponse
   JSON.mapping(
     error: String,
     error_code: Int64,
@@ -40,7 +40,7 @@ class ScrumAuthErrorResponse
   )
 end
 
-class ScrumAuthSuccessResponse
+class AuthMessage1SuccessResponse
   JSON.mapping(
     authentication: String,
     success: Bool
@@ -62,6 +62,15 @@ class ScrumAuthSuccessResponse
   end
 end
 
+class AuthMessage3
+  def initialize(nonce : String, encoded_password : String)
+    @authentication = "c=biws,r=#{nonce},p=#{encoded_password}"
+  end
+  JSON.mapping(
+    authentication: String
+  )
+end
+
 class Connection
 
   V1_0 = 0x34c2bdc3_u32
@@ -75,17 +84,46 @@ class Connection
 
   def authorise(user : String, password : String)
     client_nonce = Random::Secure.base64(14)
-    write((ScrumAuthMessage1.new(0, "SCRAM-SHA-256", "n,,n=#{user},r=#{client_nonce}").to_json + "\0").to_slice)
+    message1 = "n,,n=#{user},r=#{client_nonce}"
+    write((AuthMessage1.new(0, "SCRAM-SHA-256", message1).to_json + "\0").to_slice)
     # data = read
     json = JSON.parse(data = read)
 
     if(json["success"].as_bool)
-      response = ScrumAuthSuccessResponse.from_json(data)
+      response = AuthMessage1SuccessResponse.from_json(data)
       iteration_count = response.i
       salt = response.s
       combined_nonce = response.r
+
+      client_key = sha256(hmac_sha256(pbkdf2_hmac_sha256(password.to_slice, salt, iteration_count), "Client Key"))
+      message3_start = "c=biws,r=#{combined_nonce}"
+      auth_message = message1[3..-1] + "," + response.authentication + "," + message3_start
+      client_signature = hmac_sha256(client_key, auth_message)
+      client_proof = Bytes.new(client_signature.size)
+      client_proof.size.times do |i|
+        client_proof[i] = client_key[i] ^ client_signature[i]
+      end
+
+      message3 = AuthMessage3.new(combined_nonce, Base64.strict_encode(client_proof))
+
+      p message3.to_json
+      # password_hash = pbkdf2_hmac_sha256(password.to_slice, salt, iter)
+      #
+      # message3_start = "c=biws,r=#{nonce_c}#{nonce_s}"
+      #
+      # client_key = hmac_sha256(password_hash, "Client Key")
+      # stored_key = sha256(client_key)
+      # auth_message = message1[3..-1] + "," + message2 + "," + message3_start
+      # client_signature = hmac_sha256(stored_key, auth_message)
+      # client_proof = Bytes.new(client_signature.size)
+      # client_proof.size.times do |i|
+      #   client_proof[i] = client_key[i] ^ client_signature[i]
+      # end
+      #
+      # message3 = "c=biws,r=#{nonce_c}#{nonce_s},p=#{Base64.strict_encode(client_proof)}"
+      #
     else
-      error = ScrumAuthErrorResponse.from_json(data)
+      error = AuthMessage1ErrorResponse.from_json(data)
       raise ReqlError::ReqlDriverError::ReqlAuthError.new("error_code: #{error.error_code}, error: #{error.error}")
     end
 
