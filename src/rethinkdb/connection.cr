8155487b5cdbced9fd84cc242d459bcb9cd3b015
Caspian Baska <caspianbaska@gmail.com>
2020-03-06 09:56:23 +1100

    cancellable cursors
    `Cursor#stop` allows cleans up long-running connections like changefeeds

--- src/rethinkdb/connection.cr
+++ src/rethinkdb/connection.cr
@@ -414,26 +414,44 @@ module RethinkDB
   class Cursor
     include Iterator(QueryResult)
 
+    private property index : Int32 = 0
+    private property stopped : Bool = false
+    private property response
+    private getter stream
+
     def initialize(@stream : Connection::ResponseStream, @response : Connection::Response)
-      @index = 0
+    end
+
+    def stop
+      self.stopped = true
+      stream.conn.delete_channel(stream.id).try &.close
+      super
     end
 
     def fetch_next
-      @response = @stream.query_continue
-      @index = 0
+      self.response = stream.query_continue
+      self.index = 0
 
-      unless @response.t == ResponseType::SUCCESS_SEQUENCE || @response.t == ResponseType::SUCCESS_PARTIAL
-        raise ReqlDriverError.new("Expected SUCCESS_SEQUENCE or SUCCESS_PARTIAL but got #{@response.t}")
+      unless response.t.in? [ResponseType::SUCCESS_SEQUENCE, ResponseType::SUCCESS_PARTIAL]
+        raise ReqlDriverError.new("Expected SUCCESS_SEQUENCE or SUCCESS_PARTIAL but got #{response.t}")
       end
+
+      true
+    rescue e
+      # Do not raise after iteration stonpped
+      raise e unless stopped
+
+      false
     end
 
     def next
-      while @index == @response.r.size
-        return stop if @response.t == ResponseType::SUCCESS_SEQUENCE
-        fetch_next
+      return stop if stopped
+      while index == response.r.size
+        return stop if response.t == ResponseType::SUCCESS_SEQUENCE
+        return stop unless fetch_next
       end
 
-      value = @response.r[@index]
+      value = response.r[index]
       @index += 1
       value
     end
